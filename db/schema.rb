b879963b746550b1ffee8f39fe70a85466c26ac7
adebadayo <adebayanen@gmail.com>
2021-08-28 15:07:07 +0900

    training ActiveMailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_125515) do
+ActiveRecord::Schema.define(version: 2021_08_28_060436) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2021_08_27_125515) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,8 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+  def welcome
+    UserMailer.with(to: "test@example.com", name: 'hoge').welcome
+
+  end
+
+end

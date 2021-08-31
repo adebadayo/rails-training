4c6a3eea08b832b61db56b07d9a7dbda0bb1c33d
adebadayo <adebayanen@gmail.com>
2021-08-31 09:16:38 +0900

    training ActiveMailer

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'sidekiq'
+
+gem 'rexml', '~> 3.2', '>= 3.2.4'
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,13 +1,15 @@
 require "test_helper"
 
 class UserMailerTest < ActionMailer::TestCase
-  email = UserMailer.with(to:"igarashi@example.com",name:"igaiga").welcome
-  #メールが送信キューに追加されるかどうかをテスト
-  assert_emails(1){email.deliver_now} #❶
-  # #送信されたメールを確認するテスト#❷
-  assert_equal ["perfect_rails@example.com"], email.from
-  assert_equal ["igarashi@example.com"], email.to
-  assert_equal "登録完了", email.subject
-  assert_includes email.text_part.decoded, "igaiga"
-  assert_includes email.html_part.decoded, "igaiga"
+  test 'welcome' do
+    email = UserMailer.with(to:"igarashi@example.com",name:"igaiga").welcome
+    #メールが送信キューに追加されるかどうかをテスト
+    assert_emails(1){email.deliver_now} #❶
+    # #送信されたメールを確認するテスト#❷
+    assert_equal ["from@example.com"], email.from
+    assert_equal ["igarashi@example.com"], email.to
+    assert_equal "登録完了", email.subject
+    assert_includes email.text_part.decoded, "igaiga"
+    assert_includes email.html_part.decoded, "igaiga"
+  end
 end
